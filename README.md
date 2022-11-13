e31e4aa25d861354431ebbd0bad516f3081aacc9
Jett Jackson <jett@lumity.com.au>
2022-11-13 16:04:52 +0800

    Added config.toml; seperated out functions; report pagination; report filtering on returned results

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ cowtoot is a moderative/administrative tool that provides internal reports
 ![image](https://user-images.githubusercontent.com/22439214/201500039-06788028-2471-4dae-80a4-1d205d24c91b.png)
 
 A mastodon moderation script written by @apiratemoo and @safiire
+
+## Getting started
+
+To install the dependancies run: `pip install -r requirements.txt`
+
+Copy config.example.toml to config.toml and enter in your access token (changes to config.toml are gitignored, so will not be commited)
+
+To run the script: `python cowtoot.py`
--- config.py
+++ config.py
@@ -3,7 +3,18 @@
 2. A client secret
 3. Your access token
 
-We only need your access token in order for this script to work"""
+We only need your access token in order for this script to work
 
-#Place your access key here
-token = ""
+Add it into the config.toml
+
+"""
+
+import toml
+
+try:
+    with open('config.toml', 'r') as f:
+        config = toml.loads(f.read())
+except FileNotFoundError:
+    print('Couldn\'t find the config file! Read the README.md for instructions on setting this up')
+except Exception as e:
+    print('Something went wrong loading the config file: {e}')
