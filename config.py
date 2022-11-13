eac4f61c58eddf10204f618bff5666c259ee2859
moo <piratemoo@protonmail.com>
2022-11-12 18:33:11 -0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+""" In your profile settings, when you add an app on Mastodon, you receive
+1. A client key
+2. A client secret
+3. Your access token
+
+We only need your access token in order for this script to work"""
+
+#Place your access key here
+token = ""
